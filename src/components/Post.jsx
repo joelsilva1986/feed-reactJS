57d559e18b712e757a0834ce0a9ded154a616db9
Joel Silva <joel.a.silva1986@gmail.com>
2024-06-03 13:14:58 -0300

    Foi feita a alteração de autor (Avatar, nome e profissão) buscando as informações das props, e feita a primeira parte da formatação de datas utilizando date-fns

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,37 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post (props) {
-    console.log(props)
+export function Post ({ author, publishedAt }) {
+
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    } )
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+        
+    })
+    
 
     return (
         <article className={styles.Post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/joelsilva1986.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Joel Silva</strong>
-                        <span>Electrical Engineering</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title=" 08 de Maio às 08:13h" dateTime="22-05-11 8:13:30"> Publicado há 1h </time>
+                <time title=" 08 de Maio às 08:13h" dateTime="22-05-11 8:13:30"> 
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
