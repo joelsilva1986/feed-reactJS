a1297b06b251798cd4d25fced2a4107fa8bbed6d
Joel Silva <joel.a.silva1986@gmail.com>
2024-06-05 12:01:43 -0300

    Entendendo e aplicando o conceito de Estado (useState) na adição de novos comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,8 +4,19 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+import { useState } from 'react';
+
+const comments = [
+    1,
+    2,
+    3,
+]
 
 export function Post ({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
 
     const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -16,6 +27,13 @@ export function Post ({ author, publishedAt, content }) {
         addSuffix: true,
         
     })
+
+    function handleCreateNewComment () {
+        event.preventDefault();
+
+        setComments([...comments, comments.length + 1]);
+
+    }
     
 
     return (
@@ -44,7 +62,7 @@ export function Post ({ author, publishedAt, content }) {
                })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong> Deixe seu feedback </strong>
 
                 <textarea
@@ -57,9 +75,9 @@ export function Post ({ author, publishedAt, content }) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return < Comment />
+                })}
             </div>
         </article>
     )
