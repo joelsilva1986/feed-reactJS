d5fbebbc253f7789caf176437f74df63c14dd56f
Joel Silva <joel.a.silva1986@gmail.com>
2024-06-05 10:38:03 -0300

    Finalizado a formatação de Datas, e percorrendo o array de Posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,7 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post ({ author, publishedAt }) {
+export function Post ({ author, publishedAt, content }) {
 
     const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -29,20 +29,19 @@ export function Post ({ author, publishedAt }) {
                     </div>
                 </div>
 
-                <time title=" 08 de Maio às 08:13h" dateTime="22-05-11 8:13:30"> 
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}> 
                     {publishedDateRelativeToNow}
                 </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉{' '}<a href="">jane.design/doctorcare </a> </p>
-                <p> 
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat</a>{' '}
-                </p>
+               {content.map(line => {
+                if(line.type ==='paragraph') {
+                    return <p>{line.content}</p>
+                } else if (line.type === 'link') {
+                    return <p><a href="#">{line.content}</a></p>
+                }
+               })}
             </div>
 
             <form className={styles.commentForm}>
